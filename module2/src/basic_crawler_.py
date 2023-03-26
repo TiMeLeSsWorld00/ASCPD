75f7b6949c5b42daeb108c7bbf66ac3a57e732cb
TiMeLeSsWorld00 <75478762+TiMeLeSsWorld00@users.noreply.github.com>
2023-03-26 13:04:03 +0300

    m2 80%

--- module2/src/basic_crawler_.py
+++ module2/src/basic_crawler_.py
@@ -3,20 +3,78 @@ from urllib.parse import urljoin
 import requests
 from bs4 import BeautifulSoup
 import asyncio
+import time
+
+from tqdm import tqdm
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s',
     level=logging.INFO)
 
+def get_unique_domens(urls):
+    s = set()
+    for u in urls:
+        if len(u) < 8:
+            continue
+        if u[:7] == 'http://':
+            url = u[7:].split('/')[0]
+        elif u[:8] == 'https://':
+            url = u[8:].split('/')[0]
+        elif u[:4] == 'www.':
+            url = u
+        else:
+            continue
+
+        if url[:4] == 'www.':
+            url = url[4:]
+
+        if '@' in url:
+            continue
+        s.add(url)
+    return list(s)
+
+def get_unique_documents(urls):
+    s = set()
+    for u in urls:
+        if len(u) < 8:
+            continue
+        if u[:7] == 'http://':
+            url = u[7:]
+        elif u[:8] == 'https://':
+            url = u[8:]
+        elif u[:4] == 'www.':
+            url = u
+        else:
+            continue
+
+        if url[:4] == 'www.':
+            url = url[4:]
+
+        if '@' in url:
+            continue
+
+        doc_name = url.split('.')[-1]
+        match doc_name:
+            case 'pdf':
+                s.add(url)
+            case 'doc':
+                s.add(url)
+            case 'docx':
+                s.add(url)
+
+    return list(s)
+
 class Crawler:
 
-    def __init__(self, base_url):
+    def __init__(self, base_url, domen_url):
         self.base_url = base_url
+        self.domen_url = domen_url
         self.visited_urls = []
-        self.urls_to_visit = [ self.base_url ]
+        self.urls_to_visit = [self.base_url]
 
         #stats
         self.outer_urls = []
+        self.domen_urls = []
 
     def download_url(self, url):
         headers = {
@@ -44,12 +102,20 @@ class Crawler:
             if self.base_url in url:
                 self.add_url_to_visit(url)
             else:
-                self.outer_urls.append(url)
+                if self.domen_url in url:
+                    self.domen_urls.append(url)
+                else:
+                    self.outer_urls.append(url)
 
     async def run(self):
+        t_start = time.time()
+        tqdm_ = tqdm()
         while self.urls_to_visit:
+            if time.time() - t_start > 60 * 10:
+                break
             # if len(self.visited_urls) >= 100:
             #     break
+            tqdm_.update(1)
             url = self.urls_to_visit.pop(0)
             logging.info(f'Crawling: {url}')
             try:
@@ -58,6 +124,27 @@ class Crawler:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
                 self.visited_urls.append(url)
+        print('CRAWLING DONE')
+        print("were visited", len(self.visited_urls))
+        print("not visited, but want to", len(self.urls_to_visit))
+        print("all inner domens urls: ", len(self.domen_urls))
+        print("all outer domens urls: ", len(self.outer_urls))
+        du = get_unique_domens(self.domen_urls)
+        print("unique inner domens urls: ", len(du), du)
+        ou = get_unique_domens(self.outer_urls)
+        print("unique outer domens urls: ", len(ou), ou)
+
+
+        ddu = get_unique_documents(self.domen_urls)
+        print("unique documents urls: ", len(ddu), ddu)
+
+        print("общее количество страниц и всех ссылок", len(self.visited_urls) + len(self.urls_to_visit) + len(self.domen_urls) + len(self.outer_urls))
+        print("количество внутренних страниц", len(self.visited_urls) + len(self.urls_to_visit))
+        print("количество внутренних поддоменов (уникальных)", len(du))
+        print("общее количество ссылок на внешние ресурсы", len(self.outer_urls))
+        print("количество уникальных внешних ресурсов", len(ou))
+        print("количество уникальных ссылок на файлы doc/docx/pdf", len(ddu))
+        print("количество неработающих страниц", "????????????????")
 
 if __name__ == '__main__':
 
@@ -65,7 +152,7 @@ if __name__ == '__main__':
     #https://spbu.ru
 
     ioloop = asyncio.get_event_loop()
-    tasks = [ioloop.create_task(Crawler(base_url= 'https://spbu.ru/').run())]
+    tasks = [ioloop.create_task(Crawler(base_url= 'https://spbu.ru/', domen_url= 'spbu.ru').run())]
     wait_tasks = asyncio.wait(tasks)
     ioloop.run_until_complete(wait_tasks)
     ioloop.close()
