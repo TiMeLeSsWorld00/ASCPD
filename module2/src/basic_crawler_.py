f21aab8df1e2219851ce63e3a9447685dfb9c706
TiMeLeSsWorld00 <75478762+TiMeLeSsWorld00@users.noreply.github.com>
2023-03-26 13:24:11 +0300

    Update basic_crawler_.py

--- module2/src/basic_crawler_.py
+++ module2/src/basic_crawler_.py
@@ -69,8 +69,9 @@ class Crawler:
     def __init__(self, base_url, domen_url):
         self.base_url = base_url
         self.domen_url = domen_url
-        self.visited_urls = []
-        self.urls_to_visit = [self.base_url]
+        self.visited_urls = set()
+        self.urls_to_visit = set()
+        self.urls_to_visit.add(self.base_url)
 
         #stats
         self.outer_urls = []
@@ -83,7 +84,6 @@ class Crawler:
 
     def get_linked_urls(self, url, html):
         soup = BeautifulSoup(html, 'html.parser')
-        #print(html, url, soup.find_all('a'))
         for link in soup.find_all('a'):
             path = link.get('href')
             if path and path.startswith('/'):
@@ -91,10 +91,10 @@ class Crawler:
             yield path
 
     def add_url_to_visit(self, url):
-        if url not in self.visited_urls and url not in self.urls_to_visit:
-            self.urls_to_visit.append(url)
+        if url not in self.visited_urls:
+            self.urls_to_visit.add(url)
 
-    async def crawl(self, url):
+    def crawl(self, url):
         html = self.download_url(url)
         for url in self.get_linked_urls(url, html):
             if url is None:
@@ -107,23 +107,21 @@ class Crawler:
                 else:
                     self.outer_urls.append(url)
 
-    async def run(self):
+    def run(self):
         t_start = time.time()
         tqdm_ = tqdm()
         while self.urls_to_visit:
-            if time.time() - t_start > 60 * 10:
+            if time.time() - t_start > 60 * 1:
                 break
-            # if len(self.visited_urls) >= 100:
-            #     break
             tqdm_.update(1)
-            url = self.urls_to_visit.pop(0)
+            url = self.urls_to_visit.pop()
             logging.info(f'Crawling: {url}')
             try:
-                await self.crawl(url)
+                self.crawl(url)
             except Exception:
                 logging.exception(f'Failed to crawl: {url}')
             finally:
-                self.visited_urls.append(url)
+                self.visited_urls.add(url)
         print('CRAWLING DONE')
         print("were visited", len(self.visited_urls))
         print("not visited, but want to", len(self.urls_to_visit))
@@ -151,11 +149,8 @@ if __name__ == '__main__':
     #https://www.msu.ru/
     #https://spbu.ru
 
-    ioloop = asyncio.get_event_loop()
-    tasks = [ioloop.create_task(Crawler(base_url= 'https://spbu.ru/', domen_url= 'spbu.ru').run())]
-    wait_tasks = asyncio.wait(tasks)
-    ioloop.run_until_complete(wait_tasks)
-    ioloop.close()
+    Crawler(base_url= 'https://spbu.ru/', domen_url= 'spbu.ru').run()
+
 
 
     # Crawler(base_url= 'https://spbu.ru/').run()
